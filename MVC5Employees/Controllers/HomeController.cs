124b928b89c15fd1ae5c6e96bb2517e76f3cd760
Kameron Reffitt <reffit5@ad.nmc.edu>
2020-01-30 14:05:59 -0500

    Add project files.

---
+++ MVC5Employees/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC5Employees.Models;
+
+namespace MVC5Employees.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddEmp(int empID, string empName, int empSal)
+        {
+            Employee e = new Employee()
+            {
+                Id = empID,
+                Name = empName,
+                Salary = empSal
+            };
+            return View("EmpConfirm", e);
+        }
+        public ActionResult AddEmp()
+        {
+            return View("NewEmpForm");
+        }
+        public ActionResult GrossPayCalculator()
+        {
+            return View();
+        }
+        public ActionResult mainOfficeEmployees()
+        {
+            return View();
+        }
+    }
+    
+}
---
+++ MVC5Employees/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC5Employees.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Salary { get; set; }
+
+    }
+}
